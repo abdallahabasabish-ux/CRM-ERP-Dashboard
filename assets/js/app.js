333455d3068d567dbd987c805b5124af4dcaf89c
Abdallah Abas <abdallahabasabish@gmail.com>
2026-07-12 10:34:29 +0300

    Add main application scripts to app.js

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+    <script src="utils/constants.js" type="module"></script>
+    <script src="utils/api.js" type="module"></script>
+    <script src="utils/storage.js" type="module"></script>
+    <script src="utils/validation.js" type="module"></script>
+    <script src="utils/helpers.js" type="module"></script>
+    <script src="utils/theme.js" type="module"></script>
+    <script src="utils/router.js" type="module"></script>
+
+    <!-- تحميل المكونات الأساسية -->
+    <script src="components/navbar/navbar.js" type="module"></script>
+    <script src="components/sidebar/sidebar.js" type="module"></script>
+    <script src="components/toast/toast.js" type="module"></script>
+    <script src="components/modal/modal.js" type="module"></script>
+    <script src="components/loader/loader.js" type="module"></script>
+
+    <!-- تحميل المدير الرئيسي للتطبيق -->
+    <script src="js/app.js" type="module"></script>
